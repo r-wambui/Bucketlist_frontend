66614f2d03bd0c56183845a1ce0a0e0b4badb390
r-wambui <rose.wambui@andela.com>
2017-06-15 14:10:22 +0300

    Created a login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user:FormGroup;
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) {}
 
   ngOnInit() {
+      this.user = new FormGroup({
+        username: new FormControl('', [Validators.required ]),
+        password: new FormControl('', [Validators.required ])})
   }
 
+  onSubmit(user) {
+    let data = {
+      username: this.user.get('username').value,
+      password: this.user.get('password').value
+    }
+    let response = this.loginService.loginUser(data).toPromise().then((user) => {
+    this.router.navigate(['/bucketlist']);
+  }).catch((error) => {
+      console.log('there was an error');
+    });
+ }
 }
