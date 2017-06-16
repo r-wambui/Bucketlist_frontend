15181b21f1e77dede29db8ed756b7ae07764c8ca
r-wambui <rose.wambui@andela.com>
2017-06-16 11:36:15 +0300

    Added toPromise function

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { BucketlistService } from './bucketlist.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
-  selector: 'app-rose',
+  selector: 'bucketlist',
   templateUrl: './bucketlist.component.html',
   styleUrls: ['./bucketlist.component.css']
 })
 export class BucketlistComponent implements OnInit {
 
-  constructor() { }
+  bucketlists: BucketlistComponent[];
+
+  constructor(private BucketlistService:BucketlistService) {}
 
   ngOnInit() {
+  	this.loadBuckets();
+      
   }
 
+  loadBuckets() {
+  	this.BucketlistService.getBucketlist().toPromise().then((bucketlists) => {
+  		this.bucketlists=bucketlists;
+  		console.log(this.bucketlists); // not null
+    })
 }
+  
+
+  	
+  }
+
+
+
+  
